7cac4daecf9232167ada89e42e7a173340df8ea7
RedstoneWizard08 <jacob1coder@gmail.com>
2024-11-15 18:06:38 -0800

    Playing bug whack-a-mole

--- common/src/main/java/org/stardustmodding/interstellar/mixin/skyengine/client/LevelRendererMixin.java
+++ common/src/main/java/org/stardustmodding/interstellar/mixin/skyengine/client/LevelRendererMixin.java
@@ -1,8 +1,8 @@
 package org.stardustmodding.interstellar.mixin.skyengine.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Camera;
@@ -10,7 +10,9 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.TickRateManager;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.AABB;
@@ -18,12 +20,14 @@ import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import org.joml.Matrix4fStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.stardustmodding.skyengine.client.renderer.BlockOutlineRenderer;
 import org.stardustmodding.skyengine.entity.ShipEntity;
 import org.stardustmodding.skyengine.math.PosExt;
@@ -40,13 +44,16 @@ public abstract class LevelRendererMixin {
     @Final
     private Minecraft minecraft;
 
+    @SuppressWarnings("InvalidInjectorMethodSignature") // IntelliJ doesn't know about the captured vars
     @Inject(
-            method = "renderLevel", at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/LevelRenderer;checkPoseStack(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
-            ordinal = 2,
-            shift = At.Shift.AFTER
-    )
+            method = "renderLevel",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;checkPoseStack(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
+                    ordinal = 2,
+                    shift = At.Shift.AFTER
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
     )
     public void renderCustomBlockHitbox(
             DeltaTracker deltaTracker,
@@ -57,12 +64,22 @@ public abstract class LevelRendererMixin {
             Matrix4f frustumMatrix,
             Matrix4f projectionMatrix,
             CallbackInfo ci,
-            @Local ProfilerFiller profilerFiller,
-            @Local PoseStack poseStack,
-            @Local MultiBufferSource.BufferSource bufferSource,
-            @Local(name = "d") double camX,
-            @Local(name = "e") double camY,
-            @Local(name = "g") double camZ
+            TickRateManager tickRateManager,
+            float partialTicks,
+            ProfilerFiller profilerFiller,
+            Vec3 camPos,
+            double camX,
+            double camY,
+            double camZ,
+            boolean hasCapturedFrustum,
+            Frustum frustum,
+            float renderDistance,
+            boolean shouldHaveFog,
+            Matrix4fStack modelViewStack,
+            boolean glowingThing,
+            PoseStack poseStack,
+            MultiBufferSource.BufferSource bufferSource,
+            ObjectIterator<Entity> entitiesToRender
     ) {
         assert minecraft.player != null;
         assert level != null;
